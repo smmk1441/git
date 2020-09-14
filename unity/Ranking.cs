e36a620887e4438958a1ee79ef40b60267dae631
smmk1441 <55431228+smmk1441@users.noreply.github.com>
2020-09-14 18:01:37 +0900

    Add files via upload

---
+++ unity/Ranking.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Ranking : MonoBehaviour
+{
+
+    string url = "http://localhost:3000/rank/";
+
+    // ランク１
+    public Text rank1;
+    // ランク２
+    public Text rank2;
+    // ランク３
+    public Text rank3;
+
+    void Start()
+    {
+        StartCoroutine("GetRanking");
+    }
+
+    [System.Obsolete]
+    IEnumerator GetRanking()
+    {
+        var www = new WWW(url);
+        //wwwの値が返ってくるまで処理を止める。
+        yield return www;
+
+        //Jsonデジリアライズ
+        List<Rank> deserialized = JsonConvert.DeserializeObject<System.Collections.Generic.List<Rank>>(www.text);
+
+        //Scoreソート処理
+        List<Rank> rankingList = new List<Rank>();
+        rankingList.Add(new Rank());
+
+        foreach (Rank rank in deserialized)
+        {
+            int ichi = 0;
+
+            for (int i = 0; i < rankingList.Count; i++)
+            {
+                if (rankingList[i].score < rank.score) {
+                    ichi = i;
+                    break;
+                }
+                ichi = i + 1;
+            }
+            rankingList.Insert(ichi, rank);
+        }
+
+        //表示
+        rank1.text = "No1　" + rankingList[0].name + "  " + rankingList[0].score;
+        rank2.text = "No2　" + rankingList[1].name + "  " + rankingList[1].score;
+        rank3.text = "No3　" + rankingList[2].name + "  " + rankingList[2].score;
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Ranking");
+    }
+}
---
+++ unity/Score.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using LitJson;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // スコアを表示するGUIText
+    public Text scoreGUIText;
+
+    // ハイスコアを表示するGUIText
+    public Text highScoreGUIText;
+
+    // スコア
+    private int score;
+
+    // ハイスコア
+    private int highScore;
+
+    // PlayerPrefsで保存するためのキー
+    private string highScoreKey = "highScore";
+
+    // API呼び出しURL
+    string url = "http://localhost:3000/rank/";
+    int idcnt;
+    private string name = "";
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        // スコアがハイスコアより大きければ
+        if (highScore < score)
+        {
+            highScore = score;
+        }
+
+        // スコア・ハイスコアを表示する
+        scoreGUIText.text = score.ToString();
+        highScoreGUIText.text = "HighScore : " + highScore.ToString();
+    }
+
+    // ゲーム開始前の状態に戻す
+    private void Initialize()
+    {
+        // スコアを0に戻す
+        score = 0;
+
+        // ハイスコアを取得する。保存されてなければ0を取得する。
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+    }
+
+    // ポイントの追加
+    public void AddPoint(int point)
+    {
+        score = score + point;
+    }
+
+    // ハイスコアの保存
+    public void Save()
+    {
+        // ハイスコアを保存する
+        PlayerPrefs.SetInt(highScoreKey, highScore);
+        PlayerPrefs.Save();
+    }
+
+
+    // スコアを登録する
+    [Obsolete]
+    IEnumerator ScoreRegistByName()
+    {
+
+        var www = new WWW(url);
+        //wwwの値が返ってくるまで処理を止める。
+        yield return www;
+
+        //Jsonデジリアライズ
+        List<Rank> deserialized = JsonConvert.DeserializeObject<System.Collections.Generic.List<Rank>>(www.text);
+
+        //IDの最大を取得
+        foreach (Rank rankk in deserialized)
+        {
+            if (this.idcnt < rankk.id)
+            {
+                Debug.Log(idcnt);
+
+                this.idcnt = rankk.id;
+            }
+        }
+
+
+        Rank rank = new Rank();
+        rank.name = name;
+        rank.score = score;
+        rank.id = idcnt + 1;
+
+        var json = JsonConvert.SerializeObject(rank);
+
+        // HEADERはHashtableで記述
+        Hashtable header = new Hashtable();
+        // jsonでリクエストを送るのへッダ例
+        header.Add("Content-Type", "application/json; charset=UTF-8");
+
+        // シリアライズする(LitJson.JsonData→JSONテキスト)
+        byte[] postBytes = Encoding.Default.GetBytes(json);
+
+        // 送信開始
+        www = new WWW(url, postBytes, header);
+        yield return www;
+
+        // 成功
+        if (www.error == null)
+        {
+            Debug.Log("Post Success");
+        }
+        // 失敗
+        else
+        {
+            Debug.Log("Post Failure");
+        }
+        // ゲーム開始前の状態に戻す
+        Initialize();
+
+        SceneManager.LoadScene("Menu");
+    }
+
+    internal void ScoreRegist(string name)
+    {
+        this.name = name;
+        StartCoroutine("ScoreRegistByName");
+    }
+}
